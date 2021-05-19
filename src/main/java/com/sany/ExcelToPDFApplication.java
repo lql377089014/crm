c797eed2d4d40968f40b0d27ed748a1d9ecaceb5
lql377089014 <377089014@qq.com>
2021-05-19 10:30:10 +0800

    0519

---
+++ src/main/java/com/sany/ExcelToPDFApplication.java
@@ -0,0 +1,13 @@
+package com.sany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelToPDFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelToPDFApplication.class, args);
+		
+	}
+}
